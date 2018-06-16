b7bdaef3ada44868c2de927cad31a9ff49372712
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2018-06-16 16:35:57 +0900

    Add JSONPlaceholderViewerApi target

---
+++ JSONPlaceholderViewerApi/JSONPlaceholderViewerApi.h
@@ -0,0 +1,19 @@
+//
+//  JSONPlaceholderViewerApi.h
+//  JSONPlaceholderViewerApi
+//
+//  Created by Yoshikuni Kato on 6/16/18.
+//  Copyright © 2018 Yoshikuni Kato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JSONPlaceholderViewerApi.
+FOUNDATION_EXPORT double JSONPlaceholderViewerApiVersionNumber;
+
+//! Project version string for JSONPlaceholderViewerApi.
+FOUNDATION_EXPORT const unsigned char JSONPlaceholderViewerApiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONPlaceholderViewerApi/PublicHeader.h>
+
+
