cf6f58bc93ca78e01b22032d35efa8d3a3ef778a
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2018-07-01 13:41:11 +0900

    Update test folder structure to add PostDetailViewModelSpec

---
+++ JSONPlaceholderViewerTests/UI/PostDetail/PostDetailViewModelSpec.swift
@@ -0,0 +1,9 @@
+//
+//  PostDetailViewModelSpec.swift
+//  JSONPlaceholderViewerTests
+//
+//  Created by Yoshikuni Kato on 7/1/18.
+//  Copyright © 2018 Yoshikuni Kato. All rights reserved.
+//
+
+import Foundation
