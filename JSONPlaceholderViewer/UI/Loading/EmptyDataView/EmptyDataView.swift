acb4ea16b38457a28e773989b1f2b3ab9c90d7bb
Yoshikuni Kato <yoshikuni.kato.g@gmail.com>
2018-07-05 15:01:54 +0900

    Add loading related views from PagingManagerSample

---
+++ JSONPlaceholderViewer/UI/Loading/EmptyDataView/EmptyDataView.swift
@@ -0,0 +1,47 @@
+//
+//  EmptyDataView.swift
+//  PagingManagerSample
+//
+//  Created by Yoshikuni Kato on 2016/06/22.
+//  Copyright © 2016年 Ohako Inc. All rights reserved.
+//
+
+import UIKit
+import ReactiveSwift
+import ReactiveCocoa
+import Result
+
+final class EmptyDataView: UIView, XibInstantiatable {
+
+    private var viewModel: EmptyDataViewModeling?
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var retryButton: UIButton!
+
+    @IBAction func retryButtonTapped(_ sender: UIButton) {
+        viewModel?.retryTappedInput()
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        instantiate()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        instantiate()
+    }
+
+    func configure(with viewModel: EmptyDataViewModeling) {
+        self.viewModel = viewModel
+        bind(viewModel)
+    }
+
+    private func bind(_ viewModel: EmptyDataViewModeling) {
+        iconImageView.image = viewModel.image
+        messageLabel.text = viewModel.message
+        iconImageView.isHidden = viewModel.isImageHidden
+        retryButton.isHidden = viewModel.isRetryButtonHidden
+    }
+}
---
+++ JSONPlaceholderViewer/UI/Loading/LoadingIndicatorView/LoadingIndicatorView.swift
@@ -0,0 +1,34 @@
+//
+//  LoadingIndicatorView.swift
+//  PagingManagerSample
+//
+//  Created by Yoshikuni Kato on 2016/06/22.
+//  Copyright © 2016年 Ohako Inc. All rights reserved.
+//
+
+import UIKit
+
+final class LoadingIndicatorView: UIView, XibInstantiatable {
+    private var viewModel: LoadingIndicatorViewModeling?
+    @IBOutlet weak var loadingMessageLabel: UILabel!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        instantiate()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        instantiate()
+    }
+
+    func configure(with viewModel: LoadingIndicatorViewModeling) {
+        self.viewModel = viewModel
+        bind(viewModel)
+    }
+
+    private func bind(_ viewModel: LoadingIndicatorViewModeling) {
+        loadingMessageLabel.text = viewModel.loadingMessage
+        loadingMessageLabel.isHidden = viewModel.loadingMessage.isEmpty
+    }
+}
